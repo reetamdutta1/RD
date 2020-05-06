4029daeff99d0f47c57e6c8de829222a7d79c6c1
reetamdutta1 <56423455+reetamdutta1@users.noreply.github.com>
2020-05-06 12:29:21 +0530

    update automated whatsapp.py

--- automated whatsapp.py
+++ automated whatsapp.py
@@ -15,7 +15,7 @@ import time
 driver=webdriver.Chrome("E:/ACADEMICS/SPYDER + JUPYTER/chromedriver")
 driver.get("https://web.whatsapp.com")
 wait=WebDriverWait(driver, 600)
-target =' "Maa JIO" '
+target =' "Name of the person whom you want to send" '
 string1 = "I am ready with my bot."
 
 x_arg='//span[contains(@title, '+ target + ')]'
@@ -27,7 +27,7 @@ for i in range(10):
 #search=driver.find_element_by_class_name('vW7d1 message-out')
 #search=driver.find_element_by_link_text('https://youtu.be/sVDHOi3Nsok')
 #search.click()
-target =' "Maa JIO" '
+target =' "Name of the person whom you want to send" '
 string2 = "Are you ?"
 
 x_arg='//span[contains(@title, '+ target + ')]'
@@ -38,7 +38,7 @@ input_box=driver.find_element_by_class_name('_1Plpp')
 for i in range(10):
     input_box.send_keys(string2+Keys.ENTER)
 
-target =' "Maa JIO" '    
+target =' "Name of the person whom you want to send" '    
 string3 = "If not then be ready !"
 
 x_arg='//span[contains(@title, '+ target + ')]'
