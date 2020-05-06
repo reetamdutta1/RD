3871d3f2b38df27a649830f9152ff9c85de352c6
Reetam Dutta <reetamdutta1@gmail.com>
2020-05-06 12:43:43 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -7,7 +7,7 @@ Created on Wed May  6 12:40:04 2020
 #if you are using Anaconda, then you have to pip install two python libraries -> pyqrcode and pypng
 
 import pyqrcode
-x="https://www.youtube.com/channel/UCwpOnf144tZdz5FOaYbRFKg/featured?view_as=subscriber"
+x="Enter the text/link that you want to include in the qr code"
 qr=pyqrcode.create(x)
 qr.png("qr.png", scale =2)
 qr.show()
