0ec45117d7a9ba0742ae8292f534dc469d42cc48
reetamdutta1 <56423455+reetamdutta1@users.noreply.github.com>
2020-05-06 12:26:13 +0530

    Add files via upload

---
+++ automated messenger.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 18 21:37:32 2020
+
+@author: REETAM
+"""
+
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+driver=webdriver.Chrome("E:/ACADEMICS/SPYDER + JUPYTER/chromedriver")
+driver.get("https://messenger.com")
+#wait=WebDriverWait(driver, 600)
+#elem=browser.find_element_by_link_text('Photos')
+#elem.click()
+driver.implicitly_wait(5)
+search=driver.find_element_by_id('email')
+search.send_keys('7596985078')
+driver.implicitly_wait(5)
+search=driver.find_element_by_id('pass')
+search.send_keys('chattingrdbot')
+
+#elem=browser.find_element_by_link_text('Log In')
+#elem.click()
+driver.implicitly_wait(5)
+search=driver.find_element_by_id('loginbutton')
+search.click()
+target =' "Tathagata Dey" '
+string1 = "I am ready with my bot."
+
+#x_arg='//span[contains(@title, '+ target + ')]'
+target=wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))    
+target.click()
+input_box=driver.find_element_by_class_name('_30yy _7oam')
+for i in range(10):
+    input_box.send_keys(string1+Keys.ENTER)
---
+++ automated whatsapp.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 18 21:37:32 2020
+
+@author: REETAM
+"""
+
+
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
+driver=webdriver.Chrome("E:/ACADEMICS/SPYDER + JUPYTER/chromedriver")
+driver.get("https://web.whatsapp.com")
+wait=WebDriverWait(driver, 600)
+target =' "Maa JIO" '
+string1 = "I am ready with my bot."
+
+x_arg='//span[contains(@title, '+ target + ')]'
+target=wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))    
+target.click()
+input_box=driver.find_element_by_class_name('_1Plpp')
+for i in range(10):
+    input_box.send_keys(string1+Keys.ENTER)
+#search=driver.find_element_by_class_name('vW7d1 message-out')
+#search=driver.find_element_by_link_text('https://youtu.be/sVDHOi3Nsok')
+#search.click()
+target =' "Maa JIO" '
+string2 = "Are you ?"
+
+x_arg='//span[contains(@title, '+ target + ')]'
+target=wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))    
+target.click()
+input_box=driver.find_element_by_class_name('_1Plpp')
+
+for i in range(10):
+    input_box.send_keys(string2+Keys.ENTER)
+
+target =' "Maa JIO" '    
+string3 = "If not then be ready !"
+
+x_arg='//span[contains(@title, '+ target + ')]'
+target=wait.until(EC.presence_of_element_located((By.XPATH, x_arg)))    
+target.click()
+input_box=driver.find_element_by_class_name('_1Plpp')
+
+for i in range(10):
+    input_box.send_keys(string3+Keys.ENTER)
